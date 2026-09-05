c57cedbec9f07fe5b9ba3aca301ab05f61256d09
HAK1580 <aliofficial1580630@gmail.com>
2026-09-05 15:37:27 +0500

    updated cv stuff

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -12,7 +12,7 @@ const Hero = () => {
                 <h1 className=' sm:text-[1.5rem] md:text-[1.8rem] lg:text-[2.6vw] lg:font-bold text-[1.10rem] '>a Front End Developer.</h1>
               
                 <div className="cv-btn mt-10">
-                <a className=' flex justify-center z-1000 lg:py-2 lg:rounded-[8px] hover:bg-blue-700 hover:text-[#ffffff9f]  lg:text-[1.6rem] lg:w-55 items-center rounded-[5px] px-1 w-30 py-1.5 bg-blue-600 ' href="assets/Cv.pdf" download={"assets/Cv.pdf"}  > Download CV </a>
+                <a className=' flex justify-center z-1000 lg:py-2 lg:rounded-[8px] hover:bg-blue-700 hover:text-[#ffffff9f]  lg:text-[1.6rem] lg:w-55 items-center rounded-[5px] px-1 w-30 py-1.5 bg-blue-600 ' href="assets/M_Hassan_Ali_Khan_CV.pdf" download={"assets/M_Hassan_Ali_Khan_CV.pdf"}  > Download CV </a>
                  </div>
        
             </div>
